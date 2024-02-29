4d7164ab5994814ea42f7caa048cff5a10baa194
fh-dtatarkin <121056565+fh-dtatarkin@users.noreply.github.com>
2024-02-29 20:30:50 +0000

    Do-Now Solution

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ let birthYear = 1928;
 let person = [firstName, lastName, knownFor, birthYear];
 
 printButton.addEventListener('click', () => {
-// 1. Use a forEach loop to append the items in the `person` array to the "person" unordered list.
-//  - Select the array.
-//  - Add your forEach loop.
-//  - Create a list item.
-//  - Update the created list item element to the array info.
-//  - Append to the person list.
-  
+  // 1. Use a forEach loop to append the items in the `person` array to the "person" unordered list.
+  //  - Select the array.
+  //  - Add your forEach loop.
+  //  - Create a list item.
+  //  - Update the created list item element to the array info.
+  //  - Append to the person list.
+  person.forEach(function (item) {
+    let listItem = document.createElement('li')
+    listItem.textContent = item
+    personList.append(listItem)
+  })
 })
